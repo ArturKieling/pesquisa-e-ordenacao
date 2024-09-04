3fd7454b024836b424b5c4ea1dfebd3c04082d25
ArturKieling <145726036+ArturKieling@users.noreply.github.com>
2024-09-04 08:12:14 -0300

    alunocomparacao.java

--- alunocomparacao.java
+++ alunocomparacao.java
@@ -1,8 +1,8 @@
-package TrabalhoAvaliativo;
+package Trabalho1;
 
 import java.util.Comparator;
 
-public class AlunoComparator implements Comparator<Aluno> {
+public class Alunocomparacao implements Comparator<Aluno> {
     @Override
     public int compare(Aluno a1, Aluno a2) {
         int nomeComp = a1.getNome().compareTo(a2.getNome());
